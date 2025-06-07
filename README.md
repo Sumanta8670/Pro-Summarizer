023c4542635df85c3c9e713829abc722fab58063
Sumanta Jana <janasumanta59@gmail.com>
2025-06-07 23:10:31 +0530

    data ingestion notebook added

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Pro-Summarizer
 Pro-Summarizer is an intelligent text summarization tool powered by advanced Natural Language Processing (NLP) techniques. It takes lengthy pieces of text and condenses them into concise, coherent summaries — making information easier to digest and retain.
+# WorkFlows
+1. Update config.yaml
+2. Update params.yaml
+3. Update entity
+4. Update the configuration manager in src config
+5. Update the components
+6. Update the pipeline
+7. Update the main.py
+8. Update the app.py
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from Pro_Summerizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Pro_Summerizer.logging import logger
 
-logger.info("Welcome to our custom logging")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+          logger.exception(e)
+          raise e

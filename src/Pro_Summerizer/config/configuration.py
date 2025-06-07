aaa85fa84088dcd79a71c65606c881e521c815d0
Sumanta Jana <janasumanta59@gmail.com>
2025-06-08 00:06:12 +0530

    data validation completed

--- src/Pro_Summerizer/config/configuration.py
+++ src/Pro_Summerizer/config/configuration.py
@@ -1,7 +1,7 @@
 from Pro_Summerizer.constants import *
 from Pro_Summerizer.utils.common import read_yaml, create_directories
 from Pro_Summerizer.entity import (DataIngestionConfig)
-
+from Pro_Summerizer.entity import (DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -27,3 +27,16 @@ class ConfigurationManager:
         )
         
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        
+        create_directories([config.root_dir], verbose=True)
+        
+        data_validation_config = DataValidationConfig(
+            root_dir = config.root_dir,
+            STATUS_FILE= config.STATUS_FILE,
+            ALL_REQUIRED_FILES= config.ALL_REQUIRED_FILES
+        )
+        
+        return data_validation_config
